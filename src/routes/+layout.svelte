6e3344fc504061a06db1c33036b7c44f696a204c
TsukemonoGit <mine43try@outlook.com>
2025-08-06 21:06:31 +0900

    first

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,36 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { waitNostr } from 'nip07-awaiter';
+	import { nip19 } from 'nostr-tools';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
 
 	let { children } = $props();
+
+	onMount(async () => {
+		if (browser) {
+			document.addEventListener('nlAuth', (e: Event) => {
+				const customEvent = e as CustomEvent;
+
+				console.log(customEvent);
+
+				// 現在のパスがトップページ（'/'）であるか確認
+				if (page.url.pathname === '/') {
+					const npub = nip19.npubEncode(customEvent.detail.pubkey);
+					goto(`/${npub}`);
+				}
+			});
+
+			await waitNostr(1000);
+			const initNostrLogin = await import('nostr-login');
+			await initNostrLogin.init({
+				/*options*/
+			});
+		}
+	});
 </script>
 
 <svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,12 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
 	import { browser } from '$app/environment';
-	import { writable } from 'svelte/store';
-	import { nip19 } from 'nostr-tools';
-	import { Button } from 'bits-ui';
-	import { waitNostr } from 'nip07-awaiter';
 
-	const loginUser = writable('');
+	import { Button } from 'bits-ui';
 
 	let isLoading = false;
 	let error = '';
 	let isLoggedIn = false;
 	let pubkey = '';
-
-	onMount(async () => {
-		if (browser) {
-			document.addEventListener('nlAuth', (e: Event) => {
-				const customEvent = e as CustomEvent;
-				loginUser.set(customEvent.detail.pubkey || '');
-				console.log(customEvent);
-
-				// ログイン成功時にページ遷移（npubエンコード）
-				if (customEvent.detail.pubkey) {
-					pubkey = customEvent.detail.pubkey;
-					isLoggedIn = true;
-					const npub = nip19.npubEncode(customEvent.detail.pubkey);
-					goto(`/${npub}`);
-				}
-			});
-
-			await waitNostr(1000);
-			const initNostrLogin = await import('nostr-login');
-			await initNostrLogin.init({
-				/*options*/
-			});
-		}
-	});
 
 	async function handleLogin() {
 		if (!browser) {
